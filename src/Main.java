ab2463b0e533bb5c02becd65cd3f066a18c3843b
yali <yusufali0147@hotmail.com>
2023-09-05 10:53:48 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Bir sayının basamak değerlerini toplama aracı ");
+
+        Scanner imp =new Scanner(System.in);
+        System.out.print("Bir Sayı Giriniz : ");
+        int number =imp.nextInt();
+        int basValue,result=0;
+        while (number !=0){
+            basValue=number%10;
+            result+=basValue;
+            number/=10;
+        }
+        System.out.print("Sayınızın basamak değerlerinin toplamı : "+result);
     }
 }
